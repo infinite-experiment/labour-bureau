3eefee986e5157bb988814a97196c5e2a105c2c9
sanketpandia <sanketpandia@gmail.com>
2025-05-28 23:02:40 +0530

    scripts

---
+++ git-update.sh
@@ -0,0 +1,7 @@
+git pull
+cd ../politburo
+git pull
+cd ../vizburo
+git pull
+cd ../comrade-bot
+git pull
---
+++ vizburo-script.sh
@@ -0,0 +1,2 @@
+docker compose -f docker-compose.dev.yml build --no-cache vizburo
+docker compose -f docker-compose.dev.yml up vizburo
